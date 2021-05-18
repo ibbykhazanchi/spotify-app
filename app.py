4661447d46389482990aac07b37a2daa53adfaec
Ibrahim Khajanchi <ibrahimkhajanchi@Ibrahims-MBP.fios-router.home>
2021-05-17 23:00:14 -0400

    audio and images support

--- app.py
+++ app.py
@@ -117,34 +117,26 @@ def analyzeData():
     user2.top_genres = top_genres2            
     user2.top_artists = top_artists2
 
-    top_artists1_asSet = set(top_artists1)
-    artist_intersection = top_artists1_asSet.intersection(top_artists2)
-
-    top_genres1_asSet = set(list(user1.top_genres.keys()))
-    genre_intersection = top_genres1_asSet.intersection(list(top_genres2.keys()))
-
-    print(user1.top_artists)
-    print("YO")
-    print(user2.top_artists)
 
     commonArtist_dict = findCommonDict(top_artists1, top_artists2)
     commonSongs_dict = findCommonDict(user1.top_tracks, user2.top_tracks)
-
     commonArtist_dict = dict(sorted(commonArtist_dict.items(), key=lambda item: item[1]))
     commonSongs_dict = dict(sorted(commonSongs_dict.items(), key=lambda item: item[1]))
-
-    print(commonArtist_dict)
-
     commonArtist_list = (list) (commonArtist_dict.keys())
     commonSongs_list = (list) (commonSongs_dict.keys())
 
     
     artist_intersection_tuples = []
-    for artist in artist_intersection:
-        artist_intersection_tuples.append((artist, user1.artist_dump.get(artist)[2]))
+    for artist in commonArtist_list:
+        artist_intersection_tuples.append((artist, user1.artist_dump.get(artist)[2], (user1.top_artists.index(artist) + 1), (user2.top_artists.index(artist)) + 1))
 
-    print(artist_intersection_tuples)
-    return render_template('results.html', url1 = user1.profile_pic, url2 = user2.profile_pic, user1 = user1, user2 = user2, intersection = artist_intersection, genre_intersection = genre_intersection)
+    
+    song_intersection_tuples = []
+    for song in commonSongs_list:
+        song_intersection_tuples.append((song, user1.song_dump[song][1]))
+    
+
+    return render_template('results.html', url1 = user1.profile_pic, url2 = user2.profile_pic, user1 = user1, user2 = user2, intersection = artist_intersection_tuples, song_intersection = song_intersection_tuples)
     
     
 def findCommonDict(list1, list2):
@@ -167,7 +159,6 @@ def getTracks():
  
     
     sp = spotipy.Spotify(auth=token_info['access_token'])
-    print(sp.me())
     all_artists = []
     all_songs = []
     songs1 = []
@@ -181,14 +172,14 @@ def getTracks():
         iter+=1
         if(len(artists) < 20 and len(songs) < 20):
             break
-    
+
     artist_list = {}
     for item in all_artists:
         artist_list[item["name"]] = (item["genres"], item["popularity"], item['images'][0]['url'])
 
-    song_list = []
+    song_list = {}
     for item in all_songs:
-        song_list.append((item["name"], item["popularity"]))
+        song_list[item["name"]] = (item["popularity"], item["preview_url"])
     
 
     if userOneGo:
@@ -200,8 +191,8 @@ def getTracks():
         user1.artist_dump = artist_list
         user1.song_dump = song_list
 
-        for song in user1.song_dump:
-            songs1.append(song[0])
+        for song in user1.song_dump.keys():
+            songs1.append(song)
         user1.top_tracks = songs1
         return redirect(url_for("secondUser", _external=False))
     else:
@@ -211,11 +202,10 @@ def getTracks():
             user2.profile_pic = "/static/blank-user.jpg"
         else:
             user2.profile_pic = sp.me()['images'][0]['url']
-            print(user2.profile_pic)
         user2.artist_dump = artist_list
         user2.song_dump = song_list
-        for song in user2.song_dump:
-            songs2.append(song[0])
+        for song in user2.song_dump.keys():
+            songs2.append(song)
         user2.top_tracks = songs2
         return redirect(url_for("analyzeData", __exernal=False))
 
